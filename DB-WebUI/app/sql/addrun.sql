7a0b6ebaed8b64a202f3abc9c3bff8ba63d0eaf8
Connor Brinkman <connorbrinkman1@gmail.com>
2025-05-12 18:22:47 +0000

    bug fix

--- DB-WebUI/app/sql/addrun.sql
+++ DB-WebUI/app/sql/addrun.sql
@@ -1,4 +1,4 @@
-INSERT INTO Run (id, level_id, score, run_time) VALUES (
+INSERT INTO Run (user_id, level_id, score, run_time) VALUES (
     (SELECT id FROM "User" WHERE id = %s),
     (SELECT id FROM Level WHERE id = %s),
     %s,
